c7bc32ea7b4a7e2b35c693b566c5390c5c7b528f
LucasLofqvist <lucas.lofqvist@gmail.com>
2026-03-24 20:57:51 +0100

    Added router dependency and began implementing LoginForm and LoginView

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+
+const isHidden = ref(true);
+</script>
+
+<template>
+  <form>
+    <input type="email" placeholder="Email" />
+    <input :type="isHidden ? 'password' : 'text'" placeholder="Password" />
+
+    <section>
+      <button type="submit">Login</button>
+      <!-- <img src="" alt=""> Replace hidden-button with image later -->
+      <button type="button" @click="isHidden = !isHidden">
+        {{ isHidden ? "Show" : "Hide" }}
+      </button>
+    </section>
+
+    <section>
+      <p>
+        Don't have an account?
+        <!-- <router-link to="/register">
+            Register here.
+        </router-link> -->
+        <a href="/register">Register here.</a>
+      </p>
+      <!-- <router-link to="/forgot-password">
+        Forgot password?
+      </router-link> -->
+      <a href="/forgot-password">Forgot password?</a>
+    </section>
+  </form>
+</template>
+
+<style></style>
