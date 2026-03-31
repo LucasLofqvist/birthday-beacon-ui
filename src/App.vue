0a11ba968860cae5a15bd3b66fab823522ccf88a
LucasLofqvist <lucas.lofqvist@gmail.com>
2026-03-31 10:38:37 +0200

    added a Header component and a MainView

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
+import MainView from "./views/MainView.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <MainView />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <h1>Birthday Beacon</h1>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  border-bottom: 2px solid darkgray;
+  border-radius: 3px;
+  font-size: 14px;
+}
+</style>
