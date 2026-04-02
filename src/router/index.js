a589566ef7287302496bd7cb93c080adcf6a3932
LucasLofqvist <lucas.lofqvist@gmail.com>
2026-04-02 11:20:22 +0200

    Implemented a router and a DashboardView to test the routing.

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Layouts
+import MainLayout from "../layouts/MainLayout.vue";
+import AuthLayout from "../layouts/AuthLayout.vue";
+
+// Main Views
+import DashboardView from "../views/DashboardView.vue";
+
+// Auth Views
+import LoginView from "../views/LoginView.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: MainLayout,
+    children: [
+      {
+        path: "",
+        component: DashboardView,
+      },
+      // I will add more routes later, contacts, settings, etc.
+    ],
+  },
+  {
+    path: "/login",
+    component: AuthLayout,
+    children: [
+      {
+        path: "",
+        component: LoginView,
+      },
+      // Here I will add routes like register later.
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
