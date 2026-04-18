20dc4e3686e18b51f4a519bde88eeba6bf086ecc
LucasLofqvist <lucas.lofqvist@gmail.com>
2026-04-18 11:28:52 +0200

    Changed list key to itemIds instead of index for consistent dynamic rendering. Limited length of rendered list. Added test items.

--- src/components/SavedList.vue
+++ src/components/SavedList.vue
@@ -4,16 +4,67 @@ import { ref } from "vue";
 // Place holders for now, will be replaced with actual data from backend
 const items = ref([
   {
+    itemId: 1,
     title: "World of Warcraft Poster",
     price: "$19.99",
     link: "https://displate.com/displate/5721669",
   },
   {
+    itemId: 2,
     title: "Skyrim Map Poster",
     price: "$24.99",
     link: "N/A",
   },
   {
+    itemId: 3,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 4,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 5,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 6,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 7,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 8,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 9,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 10,
+    title: "Cyberpunk 2077 Print",
+    price: "$29.99",
+    link: "N/A",
+  },
+  {
+    itemId: 11,
     title: "Cyberpunk 2077 Print",
     price: "$29.99",
     link: "N/A",
@@ -23,9 +74,13 @@ const items = ref([
 
 <template>
   <div class="ranked-list">
-    <div v-for="(item, index) in items" :key="index" class="ranked-item">
+    <div
+      v-for="(item, index) in items.slice(0, 10)"
+      :key="item.itemId"
+      class="ranked-item"
+    >
       <div class="row">
-        <div class="rank">#{{ index + 1 <= 10 ? index + 1 : 10 }}</div>
+        <div class="rank">#{{ index + 1 }}</div>
 
         <div class="title">
           {{ item.title }}
@@ -38,7 +93,7 @@ const items = ref([
         <a class="link" :href="item.link" target="_blank"> View </a>
       </div>
 
-      <hr v-if="index !== items.length - 1" />
+      <hr v-if="index !== 9" />
     </div>
   </div>
 </template>
